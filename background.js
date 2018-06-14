bc2d84bf5c6e2e1fafb5d6e60ee736db9ffbd192
Ron Laniado <rlaniado24@gmail.com>
2018-06-14 13:46:39 -0400

    Adjusments

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onConnect.addListener(function(port) {
+    let port2 = chrome.runtime.connect({name: "timer2"});
     console.assert(port.name == "timer");
     port.onMessage.addListener(function(msg) {
         let time = msg.time;
         const startInterval = setInterval(() => {
-            port.postMessage({
+            port2.postMessage({
                 time: time - 1
             });
             time--;
-            console.log(time);
         }, 1000);
 
     });
-})
+});
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let timerTime = 60;
 let port = chrome.runtime.connect({name: "timer"});
-port.postMessage({init: "init", time: timerTime});
+port.postMessage({time: timerTime});
+chrome.runtime.onConnect(function(port2) {
+    console.assert(port2.name == "timer2");
+    port2.onMessage.addListener(function (msg) {
+        console.log(msg.time);
+    });
+});
