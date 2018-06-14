3a5cbcad55fd588e43ed6556315b1e4d81e49d38
Ron Laniado <rlaniado24@gmail.com>
2018-06-14 13:53:35 -0400

    More

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
 chrome.runtime.onConnect.addListener(function(port) {
-    let port2 = chrome.runtime.connect({name: "timer2"});
     console.assert(port.name == "timer");
     port.onMessage.addListener(function(msg) {
         let time = msg.time;
+        newPort(port);
+    });
+ });
+
+function newPort(port, time){
+    let port2 = chrome.runtime.connect({name: "timer2"});
+        port.disconnect();
         const startInterval = setInterval(() => {
             port2.postMessage({
                 time: time - 1
             });
             time--;
         }, 1000);
-
-    });
-});
+}
