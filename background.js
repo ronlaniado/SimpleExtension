399170d2e33f59c4501a95cf65645ec08a15246a
Ron Laniado <rlaniado24@gmail.com>
2018-06-15 00:50:22 -0400

    Works great!

--- background.js
+++ background.js
@@ -2,13 +2,12 @@ chrome.runtime.onConnect.addListener(function(port) {
     console.assert(port.name == "timer");
     port.onMessage.addListener(function(msg) {
         let time = msg.time;
-        newPort(port);
+        newPort(time);
     });
  });
 
-function newPort(port, time){
+function newPort(time){
     let port2 = chrome.runtime.connect({name: "timer2"});
-        port.disconnect();
         const startInterval = setInterval(() => {
             port2.postMessage({
                 time: time - 1
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let port = chrome.runtime.connect({name: "timer"});
 port.postMessage({time: timerTime});
 chrome.runtime.onConnect(function(port2) {
     console.assert(port2.name == "timer2");
-    port2.onMessage.addListener(function (msg) {
+    port2.onMessage.addListener(function(msg) {
         console.log(msg.time);
     });
 });
