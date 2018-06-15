0798c205505c839e3324fa3206eba8e467f91626
Ron Laniado <rlaniado24@gmail.com>
2018-06-14 23:51:56 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let timerTime = 60;
-let port = chrome.runtime.connect({name: "timer"});
-port.postMessage({time: timerTime});
-chrome.runtime.onConnect(function(port2) {
-    console.assert(port2.name == "timer2");
-    port2.onMessage.addListener(function (msg) {
-        console.log(msg.time);
-    });
-});
